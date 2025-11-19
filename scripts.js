73ab46966725a94e95fd559495e4fa7f6b4f3aa7
yudithleon <yudithkleon@gmail.com>
2025-11-19 13:15:20 -0500

    creando el post en js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,39 @@
-console.log("holis")
+const postForm = document.getElementById("post-form");
+const titleInput = document.getElementById("title");
+const descInput = document.getElementById("description");
+const imageInput = document.getElementById("image");
+const feed = document.getElementById("feed");
+const emptyText = document.getElementById("empty-text");
+
+const posts = [];
+postForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(e.target);
+  const title = titleInput.value.trim();
+  const description = descInput.value.trim();
+  console.log(title, description, imageInput);
+
+  const id = Date.now().toString();
+  let imageUrl = null;
+  if (imageInput.files && imageInput.files[0]) {
+    const file = imageInput.files[0];
+    imageUrl = URL.createObjectURL(file);
+  }
+
+  console.log(imageUrl);
+
+  //creemos el post
+  const post = {
+    id,
+    title,
+    description,
+    imageUrl,
+    likes: 0,
+    liked: false,
+  };
+
+  posts.unshift(post);
+console.log(posts)
+  postForm.reset();
+
+});
